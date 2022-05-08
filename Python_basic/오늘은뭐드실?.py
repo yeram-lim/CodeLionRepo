c9dae53564d3992f1f329db2061327eeb69af2a8
LimYeram <yeramyeye@gmail.com>
2022-05-09 08:22:09 +0900

    [pythonBasic] ch08

--- Python_basic/오늘은뭐드실?.py
+++ Python_basic/오늘은뭐드실?.py
@@ -1,7 +1,17 @@
+foods = ['된장찌개', '피자', '제육볶음']
+
+for x in range(3):
+    print(foods[x])
+
+for x in foods:
+    print(x)
+
 information = {
     '고향': '서울',
     '취미': '영화 관람',
     '좋아하는 음식': '떡볶이'
 }
 
-print(information.get('취미'))
+for x, y in information.items():
+    print(x)
+    print(y)

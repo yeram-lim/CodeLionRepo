3bf9e74838d2b5fe78b8807e60fc2594a6e89651
LimYeram <yeramyeye@gmail.com>
2022-05-10 08:55:05 +0900

    [pythonBasic]Part2 ch02

--- Python_basic/이상형이뭐에요?.py
+++ Python_basic/이상형이뭐에요?.py
@@ -1,16 +1,9 @@
-def make_dolcelatte():
-    print('1. 얼음을 넣는다.')
-    print('2. 연유를 30ml 넣는다.')
-    print('3. 찬 우유를 넣는다.')
-    print('4. 에스프레소샷을 넣는다.')
+total_dictionary= {}
 
-def make_blueberry_smoothie():
-    print('1. 블루베리 20g을 넣는다.')
-    print('2. 연유를 300ml 넣는다.')
-    print('3. 얼음을 넣는다.')
-    print('4. 믹서기에 간다.')
-
-def make_simple_latte():
-    print('1. 커피를 넣는다.')
-    print('2. 우유를 넣는다.')
-    print('3. 신나게 섞는다.')
+while True:
+    question = input('질문을 입력해주세요 : ')
+    if question == 'q':
+        break
+    else:
+        total_dictionary[question] = ''
+print(total_dictionary)

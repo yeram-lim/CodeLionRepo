6862f75e1c3ef3aeb884f3fcaf2a4a069462e055
LimYeram <yeramyeye@gmail.com>
2022-05-13 20:51:13 +0900

    [pythonLv2]ch2-15

--- Python_crawling/실시간검색어확인하기.py
+++ Python_crawling/실시간검색어확인하기.py
@@ -1,8 +1,19 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
 
 url = "http://www.daum.net/"
 response = requests.get(url)
-print(type(response.text))
+soup = BeautifulSoup(response.text, 'html.parser')
+rank = 1
 
-print(type(BeautifulSoup(response.text, 'html.parser')))
+results = soup.findAll('a','link_favorsch')
+
+search_rank_file = open("rankresult.txt","w")
+
+print(datetime.today().strftime("%Y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
+
+for result in results:
+    search_rank_file.write(str(rank)+"위:"+result.get_text()+"\n")
+    print(rank,"위 : ",result.get_text(),"\n")
+    rank += 1

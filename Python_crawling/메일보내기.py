a840f42420a5e2a2fa06e3115aeca1b6cc03dac1
LimYeram <yeramyeye@gmail.com>
2022-05-14 12:31:26 +0900

    [pythonLv2]ch5-10

--- Python_crawling/메일보내기.py
+++ Python_crawling/메일보내기.py
@@ -11,7 +11,12 @@ message["Subject"] = "이것은 제목입니다."
 message["From"] = "###@gmail.com"
 message["To"] = "###@gmail.com"
 
-smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
-smtp.login("###@gmail.com","######")
-smtp.send_message(message)
-smtp.quit()
+# open() - codelion.png / rb
+image = open("codelion.png", 'rb')
+# 파일을 읽어서 출력해보세요. read()
+print(image.read())
+
+# smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
+# smtp.login("###@gmail.com","######")
+# smtp.send_message(message)
+# smtp.quit()

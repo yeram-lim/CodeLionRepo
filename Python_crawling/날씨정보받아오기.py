860ebf74b2d97e46636667c5d0a66be6b84a76fb
LimYeram <yeramyeye@gmail.com>
2022-05-14 10:56:19 +0900

    [pythonLv2]ch2-10

--- Python_crawling/날씨정보받아오기.py
+++ Python_crawling/날씨정보받아오기.py
@@ -3,16 +3,23 @@ import json
 
 city = "Seoul"
 apikey = "################################"
-api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
+lang = "kr"
+# units - metric
+api = f"""http://api.openweathermap.org/data/2.5/\
+weather?q={city}&appid={apikey}&lang={lang}&units=metric"""
 
 result = requests.get(api)
 # print(result.text)
 
 data = json.loads(result.text)
 
+# 지역 : name
 print(data["name"],"의 날씨입니다.")
+# 자세한 날씨 : weather - description
 print("날씨는 ",data["weather"][0]["description"],"입니다.")
+# 현재 온도 : main - temp
 print("현재 온도는 ",data["main"]["temp"],"입니다.")
+# 체감 온도 : main - feels_like
 print("하지만 체감 온도는 ",data["main"]["feels_like"],"입니다.")
 # 최저 기온 : main - temp_min
 print("최저 기온은 ",data["main"]["temp_min"],"입니다.")

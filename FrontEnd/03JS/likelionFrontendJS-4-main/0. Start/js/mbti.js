c7781d0c9cd99a1815008c94c1e1828c7d6786b0
yeram-lim <yeramyeye@gmail.com>
2022-05-27 00:03:59 +0900

    JS 실습 result 페이지

--- FrontEnd/03JS/likelionFrontendJS-4-main/0. Start/js/mbti.js
+++ FrontEnd/03JS/likelionFrontendJS-4-main/0. Start/js/mbti.js
@@ -1,9 +1,12 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
 
 const endPoint = 10;
 
-function addAnswer(answerText,qIdx){
+const select = [0,0,0,0];
+
+function addAnswer(answerText,qIdx,idx){
     var a = document.querySelector(".aBox");
     var answer = document.createElement('button');
     
@@ -27,23 +30,82 @@ function addAnswer(answerText,qIdx){
             children[i].style.animation = "fadeOut 0.5s"
         }
         setTimeout(()=>{
+
+            var target = qnaList[qIdx].a[idx].type;
+
+            for(let i=0;i<target.length;i++){
+                select[target[i]]+=1 
+            }
+
             for(let i=0;i<children.length;i++){
                 children[i].style.display = 'none';
             }
             goNext(++qIdx);
         },450)
     },false)
+}
 
+function calResult(){
+    var result = select.indexOf(Math.max(...select));
+    return result;
+}
 
+function setResult(){
+    let point = calResult();
+    
+    const resultNameIntro = document.querySelector('.resultIntro');
+    resultNameIntro.innerHTML = infoList[point].nameIntro; 
+
+    const resultName = document.querySelector('.resultName');
+    resultName.innerHTML = infoList[point].name;
+
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector("#resultImg");
+    var imgURL = 'img/image-' + point + '.png';
+
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+    
+    const resultDesc1 = document.querySelector('.resultDesc1');
+    const resultDescTitle1 = document.querySelector('.resultDescTitle1');
+    resultDescTitle1.innerHTML = infoList[point].descTitle1;
+    resultDesc1.innerHTML = infoList[point].desc1;
+  
+    const resultDesc2 = document.querySelector('.resultDesc2');
+    const resultDescTitle2 = document.querySelector('.resultDescTitle2');
+    resultDescTitle2.innerHTML = infoList[point].descTitle2;
+    resultDesc2.innerHTML = infoList[point].desc2;
+}
+
+function goResult(){
+    qna.style.WebkitAnimation = "fadeOut 1s";
+    qna.style.animation = "fadeOut 1s";
+    setTimeout(()=>{
+        result.style.WebkitAnimation = "fadeIn 1s";
+        result.style.animation = "fadeIn 1s";
+        setTimeout(() => {
+            qna.style.display = "none";
+            result.style.display = "block";            
+        }, 450);
+    },450);
+
+    setResult();
 }
 
 function goNext(qIdx){
+    if(qIdx == endPoint){
+        goResult();
+        return;
+    }
+
     var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q;
     
     for(let i in qnaList[qIdx].a)
     {
-        addAnswer(qnaList[qIdx].a[i].answer,qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer,qIdx,i);
     }
     var countStatusNum = document.querySelector('.countStatus');
     countStatusNum.innerHTML = (qIdx+1)+"/"+endPoint;
